123e8ec678eae348af798cc4b08fdc442aac43d4
Reander Agulto <reanderagulto29@gmail.com>
2024-10-24 20:58:42 +0800

    Initial Commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Add validation to the request
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'body'  => 'required'
+        ]);
+
+        // Create post
+        Post::create($request->all());
+
+        // Redirect to index with Success message
+        return redirect()->route('posts.index')
+                ->with('success', 'Post created successfully');
+    }
+
+     /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+
+        $post = Post::find($id);
+        $post->update($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success', 'Post deleted successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified post.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::find($id);
+
+        return view('posts.edit', compact('post'));
+    }
+
+     // routes functions
+    /**
+     * Show the form for creating a new post.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+}
